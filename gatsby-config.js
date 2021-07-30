7c45c0f9e9595d615c0b22e38ef1679039ce5489
rabie lakhdar <rlakhdar@maltem.com>
2021-07-30 15:40:08 +0100

    pwa changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,11 +22,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Rabiel.dev`,
+        short_name: `Rabiel`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#02aab0`,
+        theme_color: `#793fc1`,
         display: `standalone`,
         icon: 'src/images/favicon.png',
       },
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,13 +20,7 @@ const Footer = () => {
             networks.map((network) => {
               const { id, name, url } = network;
               return (
-                <a
-                  key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  aria-label={name}
-                >
+                <a key={id} href={url} rel="noopener noreferrer" target="_blank" aria-label={name}>
                   <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
                 </a>
               );
