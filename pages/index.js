ffa8750dae931fb6be36ba434af916b0a20a0bfd
rabie lakhdar <rlakhdar@maltem.com>
2024-02-15 21:07:55 +0100

    use next

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>blockchain Developer</h1>
+      <h1>solidity Developer</h1>
+      <h1>web3 Developer</h1>
+      </div>
+  );
+}
